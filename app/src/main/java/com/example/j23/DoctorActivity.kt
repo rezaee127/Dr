66c929d973284351b55b5d1c446889abbe13030d
razie rezaee <rezaie127@yahoo.com>
2022-02-24 11:09:29 +0330

    Initial commit

---
+++ app/src/main/java/com/example/j23/DoctorActivity.kt
@@ -0,0 +1,17 @@
+package com.example.j23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.j23.databinding.ActivityDoctorBinding
+
+
+class DoctorActivity : AppCompatActivity() {
+    lateinit var binding:ActivityDoctorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor)
+        binding =ActivityDoctorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
